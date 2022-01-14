6caf3a55bf0908243cda0b6f66ee8b396251cc54
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2022-01-14 14:52:42 -0500

    separate er processing into separate function

--- FTK/extract_extents.py
+++ FTK/extract_extents.py
@@ -1,5 +1,5 @@
 import csv
-import pandas as pd
+import pandas
 from lxml import etree
 
 
@@ -12,7 +12,7 @@ def extract_file_tableids(tree):
     bf####_####
     the first #### is a shared id for all files in the bookmark
     '''
-    
+
     file_tableids = []
 
     bf_table_ids = tree.xpath(
@@ -31,7 +31,7 @@ def get_file_size(file_tableid):
     '''
     extract the file size based on its location in the file table
     '''
-    
+
     table_cell = tree.xpath(
         f'/fo:root/fo:page-sequence/fo:flow/fo:table[@id="{file_tableid}"]'\
         '/fo:table-body/fo:table-row/fo:table-cell/fo:block/text()',
@@ -40,6 +40,31 @@ def get_file_size(file_tableid):
     file_size = int(table_cell[2].replace(" B",""))
 
     return file_size
+
+
+def get_er_report(bookmark, file_tableids):
+    '''
+    extract er number, er name, byte count, and file count
+    '''
+
+    report = {}
+
+    er_components = bookmark.text.replace('Bookmark: ', '').split(':')
+    report['er_number'] = er_components[0]
+    report['er_name'] = er_components[1].strip()
+
+
+    bookmark_id = bookmark.get('id')
+    file_tableid_prefix = bookmark_id.replace('k','f')
+    report['logical_size'] = 0
+    report['file_count'] = 0
+
+    for file_tableid in file_tableids:
+        if file_tableid_prefix in file_tableid:
+            report['logical_size'] += get_file_size(file_tableid)
+            report['file_count'] += 1
+
+    return report
 
 
 def generate_report(tree, file_tableids):
@@ -55,34 +80,18 @@ def generate_report(tree, file_tableids):
     )
 
     for bookmark in bookmarks:
-        bookmark_id = bookmark.get('id')
-        bookmark_name = tree.xpath(
-            f'/fo:root/fo:page-sequence/fo:flow/fo:block[@id="{bookmark_id}"]/text()',
-            namespaces=FO_NAMESPACE
-        )
-        file_tableid_prefix = bookmark_id.replace('k','f')
-        logical_size = 0
-        file_count = 0
-
-        for file_tableid in file_tableids:
-            if file_tableid_prefix in file_tableid:
-                logical_size += get_file_size(file_tableid)
-                file_count += 1
-
-
-        if "Bookmark" in bookmark_name[0]:
-            report[bookmark_id] = [
-                bookmark_name[0].replace("Bookmark: ",""),
-                file_count,
-                logical_size
-            ]
+        if 'ER' in bookmark.text:
+            report[bookmark.get('id')] = get_er_report(bookmark, file_tableids)
+        else:
+            continue
 
     return report
+
 
 def make_csv(report):
     df = pd.DataFrame(report)
     df = df.transpose()
-    df.rename(columns={0:"Bookmark Name", 1:"File Count", 2:"Size"})
+    df.rename(columns={0:'ER Number', 1: 'ER Name', 2:'File Count', 3:'Size'})
     df.to_csv('ftk_test.csv')
 
 
