3d2334869837b6ff9882fb3233b01217df9ccbd9
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2022-01-14 12:31:28 -0500

    rename main function to main, use __name__ for CLI

--- FTK/extract_extents.py
+++ FTK/extract_extents.py
@@ -2,13 +2,6 @@ import csv
 import pandas as pd
 from lxml import etree
 
-#tree = etree.parse('/ER3-Report.xml')
-
-
-def make_ftk_report(tree):
-    prepare_report()
-    generate_report()
-    make_csv(report)
 
 def prepare_report():
     report = {}
@@ -18,6 +11,7 @@ def prepare_report():
     for item in bf_table_ids:
         if "bf" in item.get('id'):
             all_tables.append(item.get('id'))
+
 
 def generate_report():
     for bookmark in tree.xpath('/fo:root/fo:page-sequence/fo:flow/fo:block[@id]', namespaces={'fo': "http://www.w3.org/1999/XSL/Format"}):
@@ -37,8 +31,20 @@ def generate_report():
         if "Bookmark" in name[0]:
             report[bookmark_id] = [name[0].replace("Bookmark: ",""), file_count, logical_size]
 
+
 def make_csv(report):
     df = pd.DataFrame(report)
     df = df.transpose()
     df.rename(columns={0:"Bookmark Name", 1:"File Count", 2:"Size"})
     df.to_csv('ftk_test.csv')
+
+
+def main():
+    tree = etree.parse('/ER3-Report.xml')
+    prepare_report()
+    generate_report()
+    make_csv(report)
+
+
+if __name__ == '__main__':
+    main()
