ed5a85f0b6b2d16fb4f58daf560cbd1a1d6acc7d
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2023-09-12 12:23:57 -0400

    add required arguments

--- src/ipres_package_cloud/package_cloud.py
+++ src/ipres_package_cloud/package_cloud.py
@@ -1,8 +1,37 @@
+import argparse
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description='test'
+    )
+    parser.add_argument(
+        '--payload',
+        required=True
+    )
+    parser.add_argument(
+        '--log',
+        required=True
+    )
+    parser.add_argument(
+        '--md5',
+        required=True
+    )
+    parser.add_argument(
+        '--dest',
+        required=True
+    )
+    parser.add_argument(
+        '--id',
+        required=True
+    )
+
+    return parser.parse_args()
+
 def main():
+    args = parse_args()
     LOGGER.info("I do not package anything yet")
 
 if __name__ == "__main__":
--- tests/test_package_cloud.py
+++ tests/test_package_cloud.py
@@ -1,5 +1,6 @@
 import ipres_package_cloud.package_cloud as pc
 
+import argparse
 from pathlib import Path
 import pytest
 import shutil
@@ -9,3 +10,38 @@ def transfer_files(tmp_path: Path, request):
     fixture_data = Path(request.module.__file__).parent / 'fixtures'
     shutil.copytree(fixture_data, tmp_path, dirs_exist_ok=True)
     return tmp_path
+
+@pytest.fixture
+def args(transfer_files):
+    args = [
+        "script_name",
+        "--payload",
+        str(transfer_files / 'fixtures' / 'rclone_files'),
+        "--md5",
+        str(transfer_files / 'rclone.md5'),
+        "--log",
+        str(transfer_files / 'rclone.log'),
+        "--dest",
+        str(transfer_files),
+        "--id",
+        "ACQ_1234_123456"
+    ]
+    return args
+
+def test_requires_args(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture, args: list):
+    """Test that script requires all five args"""
+
+    for i in range(0, 5):
+        # remove a pair of list items (arg and value) for each test
+        part_args = args[0:2*i+1] + args[2*i+3:]
+
+        monkeypatch.setattr(
+                'sys.argv', part_args
+            )
+
+        with pytest.raises(SystemExit):
+            args = pc.parse_args()
+
+        stderr = capsys.readouterr().err
+
+        assert args[2*i+1] in stderr
