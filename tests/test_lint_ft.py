a6333e5d7cda962c29f0fe7827635e6005b1156f
hshiue <hilaryszuyinshiue@nypl.org>
2023-09-15 16:02:54 -0400

    add pytest fixture

--- tests/test_lint_ft.py
+++ tests/test_lint_ft.py
@@ -30,6 +30,38 @@ def test_directory_argument(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
 
     assert child_dir in args.packages
 
+# linting tests
+@pytest.fixture
+def good_package(tmp_path: Path):
+    pkg = tmp_path.joinpath("ACQ_1234_123456")
+    f_object_data = pkg / "objects" / "data"
+    f_object_data.mkdir(parents=True)
+
+    bag_files = ["bag-info.txt", "bagit.txt", "manifest-md5.txt", "tagmanifest-md5.txt"]
+    for f in bag_files:
+        filepath = pkg / "objects" / f
+        filepath.touch()
+        filepath.write_bytes(b"some bytes for file")
+
+    obj_data_file = f_object_data / "file_1.txt"
+    obj_data_file.touch()
+    obj_data_file.write_bytes(b"some bytes for file")
+
+    obj_data_folder = f_object_data / "folder1"
+    obj_data_folder.mkdir(parents=True, exist_ok=True)
+
+    obj_data_folder_file = obj_data_folder / "fileinfolder.txt"
+    obj_data_folder_file.touch()
+    obj_data_folder_file.write_bytes(b"some bytes for file")
+
+    f_metadata = pkg.joinpath("metadata")
+    f_metadata.mkdir()
+
+    metadata_filepath = f_metadata.joinpath("rclone.log")
+    metadata_filepath.touch()
+    metadata_filepath.write_bytes(b"some bytes for metadata")
+
+    return pkg
 
 
 
