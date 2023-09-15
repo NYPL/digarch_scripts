230f9a9eb015d3ea658f624a350062d1f9e9ff45
hshiue <hilaryszuyinshiue@nypl.org>
2023-09-15 14:42:16 -0400

    add test_package_argument and test_directory_argument

--- tests/test_lint_ft.py
+++ tests/test_lint_ft.py
@@ -3,3 +3,33 @@ from pathlib import Path
 import pytest
 
 import ipres_package_cloud.lint_ft as lint_ft
+
+# Unit tests
+# Argument tests
+def test_package_argument(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(
+        "sys.argv",
+        ["script", "--package", str(tmp_path)],
+    )
+
+    args = lint_ft.parse_args()
+
+    assert tmp_path in args.packages
+
+
+def test_directory_argument(tmp_path: Path, monkeypatch: pytest.MonkeyPatch):
+    child_dir = tmp_path / "child"
+    child_dir.mkdir()
+
+    monkeypatch.setattr(
+        "sys.argv",
+        ["script", "--directory", str(tmp_path)],
+    )
+
+    args = lint_ft.parse_args()
+
+    assert child_dir in args.packages
+
+
+
+
