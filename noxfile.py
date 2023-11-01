3dd03ea5cc8ff63bd039b3bf2b3087b5126419b6
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2023-11-01 14:21:37 -0400

    add flake8

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,12 @@ import nox
 python_versions = ["3.11", "3.10"]
 
 @nox.session(python=python_versions)
-def tests(session):
+def test(session):
     session.install(".")
     session.install("pytest")
     session.run("pytest")
+
+@nox.session(python=python_versions)
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", "src", "tests", "noxfile.py")
