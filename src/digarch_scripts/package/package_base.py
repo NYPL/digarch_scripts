014b8852c646fb52e064baadc758f37dd2754747
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2024-04-22 12:03:30 -0400

    rebase packaging functions to shared module

---
+++ src/digarch_scripts/package/package_base.py
@@ -0,0 +1,159 @@
+import argparse
+import logging
+import os
+import re
+from datetime import date
+from pathlib import Path
+
+import bagit
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+
+def parse_args() -> argparse.Namespace:
+    def extant_path(p: str) -> Path:
+        path = Path(p)
+        if not path.exists():
+            raise argparse.ArgumentTypeError(f"{path} does not exist")
+        return path
+
+    def digital_carrier_label(id: str) -> Path:
+        pattern = r"ACQ_\d{4}_\d{6}"
+        if not re.match(r"ACQ_\d{4}_\d{6}", id):
+            raise argparse.ArgumentTypeError(
+                f"{id} does not match the expected {type} pattern, {pattern}"
+            )
+        return id
+
+    parser = argparse.ArgumentParser(description="test")
+    parser.add_argument("--payload", required=True, type=extant_path)
+    parser.add_argument("--log", required=True, type=extant_path)
+    parser.add_argument("--md5", required=True, type=extant_path)
+    parser.add_argument("--dest", required=True, type=extant_path)
+    parser.add_argument("--id", required=True, type=digital_carrier_label)
+
+    return parser.parse_args()
+
+
+def create_base_dir(dest: Path, id: str) -> Path:
+    acq_id = id.rsplit("_", 1)[0]
+    package_base = dest / acq_id / id
+    if package_base.exists():
+        raise FileExistsError(
+            f"{package_base} already exists. Make sure you are using the correct ID"
+        )
+
+    try:
+        package_base.mkdir(parents=True)
+    except PermissionError:
+        raise PermissionError(f"{dest} is not writable")
+    return package_base
+
+
+def move_metadata_file(md_path: Path, pkg_dir: Path) -> None:
+    md_dir = pkg_dir / "metadata"
+    if not md_dir.exists():
+        md_dir.mkdir()
+
+    new_md_path = md_dir / md_path.name
+    if new_md_path.exists():
+        raise FileExistsError(f"{new_md_path} already exists. Not moving.")
+
+    md_path.rename(new_md_path)
+    return None
+
+
+def move_metadata_files(md_paths: list[Path], pkg_dir: Path) -> None:
+    for md_path in md_paths:
+        try:
+            move_metadata_file(md_path, pkg_dir)
+        except FileExistsError as e:
+            raise Warning(
+                f"{e} One or more metadata files may have already been moved to new location"
+            )
+    return None
+
+
+def create_bag_in_objects(payload_path: Path, md5_path: Path, pkg_dir: Path) -> None:
+    bag_dir = pkg_dir / "objects"
+    bag_dir.mkdir()
+    move_payload(payload_path, bag_dir)
+    convert_rclone_md5_to_bagit_manifest(md5_path, bag_dir)
+    # generate baginfo.txt and bagit.txt (copying code snippet from bagit)
+    create_bag_tag_files(bag_dir)
+    return None
+
+
+def move_payload(payload_path: Path, bag_dir: Path) -> None:
+    # instantiate a var for objects dir
+    payload_dir = bag_dir / "data"
+    # if the object folder does not exist create it
+    if not payload_dir.exists():
+        payload_dir.mkdir(parents=True)
+    else:
+        raise FileExistsError(f"{payload_dir} already exists. Not moving files.")
+
+    for a_file in payload_path.iterdir():
+        new_ob_path = payload_dir / a_file.name
+        # if a payload file is already in the object directory do not move, raise error
+        if new_ob_path.exists():
+            raise FileExistsError(f"{new_ob_path} already exists. Not moving.")
+
+        a_file.rename(new_ob_path)
+    return None
+
+
+def convert_rclone_md5_to_bagit_manifest(md5_path: Path, bag_dir: Path) -> None:
+    # check for manifest
+    new_md5_path = bag_dir / "manifest-md5.txt"
+    if new_md5_path.exists():
+        raise FileExistsError("manifest-md5.txt already exists, review package")
+
+    with open(md5_path, "r") as f:
+        manifest_data = f.readlines()
+
+    updated_manifest = [line.replace("  ", "  data/") for line in manifest_data]
+    # re-writes the manifest lines
+    with open(md5_path, "w") as f:
+        f.writelines(updated_manifest)
+    # move md5 file to manifest-md5.txt in bag
+    md5_path.rename(new_md5_path)
+
+    return None
+
+
+def create_bag_tag_files(bag_dir: Path):
+    txt = """BagIt-Version: 0.97\nTag-File-Character-Encoding: UTF-8\n"""
+    with open(bag_dir / "bagit.txt", "w") as bagit_file:
+        bagit_file.write(txt)
+
+    bag_info = {}
+    bag_info["Bagging-Date"] = date.strftime(date.today(), "%Y-%m-%d")
+    bag_info["Bag-Software-Agent"] = "package_cloud.py"
+    total_bytes, total_files = get_oxum(bag_dir / "data")
+    bag_info["Payload-Oxum"] = f"{total_bytes}.{total_files}"
+    bagit._make_tag_file(bag_dir / "bag-info.txt", bag_info)
+
+
+def get_oxum(payload_dir: Path) -> tuple[int, int]:
+    total_bytes = 0
+    total_files = 0
+
+    for payload_file in payload_dir.rglob("*"):
+        if payload_file.is_file():
+            total_files += 1
+            total_bytes += os.stat(payload_file).st_size
+
+    return total_bytes, total_files
+
+
+def validate_bag_in_payload(pkg_dir: Path) -> None:
+    bag_dir = pkg_dir / "objects"
+    bag = bagit.Bag(str(bag_dir))
+    try:
+        bag.validate(completeness_only=True)
+        LOGGER.info(f"{bag.path} is valid.")
+    except bagit.BagValidationError:
+        LOGGER.warning(f"{bag.path} is not valid. Check the bag manifest and oxum.")
+    return None
---
+++ tests/test_package_base.py
@@ -0,0 +1,261 @@
+import os
+import shutil
+from pathlib import Path
+
+import bagit
+import pytest
+
+import digarch_scripts.package.package_base as pb
+
+
+@pytest.fixture
+def transfer_files(tmp_path: Path, request):
+    fixture_data = Path(request.module.__file__).parent / "fixtures" / "cloud"
+    shutil.copytree(fixture_data, tmp_path, dirs_exist_ok=True)
+    return tmp_path
+
+
+@pytest.fixture
+def payload(transfer_files):
+    return transfer_files / "rclone_files"
+
+
+@pytest.fixture
+def md5_manifest(transfer_files):
+    return transfer_files / "rclone.md5"
+
+
+@pytest.fixture
+def log(transfer_files):
+    return transfer_files / "rclone.log"
+
+
+@pytest.fixture
+def id():
+    return "ACQ_1234_123456"
+
+
+def args(transfer_files):
+    args = [
+        transfer_files / "rclone.md5",
+        transfer_files / "rclone.log",
+        transfer_files,
+    ]
+    return args
+
+
+def test_create_package_basedir_exc_on_readonly(tmp_path: Path, id: str):
+    """Test that package folder maker reports permission error"""
+
+    # make folder read-only
+    os.chmod(tmp_path, 0o500)
+
+    with pytest.raises(PermissionError) as exc:
+        pb.create_base_dir(tmp_path, id)
+
+    # change back to allow clean-up (might not be necessary)
+    os.chmod(tmp_path, 0o777)
+    assert f"{str(tmp_path)} is not writable" in str(exc.value)
+
+
+def test_create_package_basedir(tmp_path: Path, id: str):
+    """Test that package folder maker makes ACQ and Carrier folders"""
+
+    base_dir = pb.create_base_dir(tmp_path, id)
+
+    assert base_dir.name == id
+    assert base_dir.parent.name == id[:-7]
+
+
+def test_create_package_basedir_with_existing_acq_dir(tmp_path: Path, id: str):
+    """Test that package folder maker respect existing ACQ folder"""
+
+    (tmp_path / id[:-7]).mkdir()
+    base_dir = pb.create_base_dir(tmp_path, id)
+
+    assert base_dir.name == id
+    assert base_dir.parent.name == id[:-7]
+
+
+def test_error_on_existing_package_dir(tmp_path: Path, id: str):
+    """Test that package folder maker errors if carrier folder exists"""
+
+    base_dir = tmp_path / id[:-7] / id
+    base_dir.mkdir(parents=True)
+
+    with pytest.raises(FileExistsError) as exc:
+        pb.create_base_dir(tmp_path, id)
+
+    assert f"{base_dir} already exists. Make sure you are using the correct ID" in str(
+        exc.value
+    )
+
+
+@pytest.fixture
+def package_base_dir(tmp_path: Path, id: str):
+    return pb.create_base_dir(tmp_path, id)
+
+
+def test_move_metadata(package_base_dir: Path, log: Path):
+    """Test that metadata folder and log file are moved successfully"""
+
+    pb.move_metadata_file(log, package_base_dir)
+
+    assert not log.exists()
+    assert (package_base_dir / "metadata" / "rclone.log").exists()
+
+
+def test_do_not_overwrite_metadata(package_base_dir: Path, log: Path):
+    """Test that log file is not moved if a same name file exists in dest"""
+
+    rclone_log = package_base_dir / "metadata" / log.name
+    rclone_log.parent.mkdir()
+    rclone_log.touch()
+
+    with pytest.raises(FileExistsError) as exc:
+        pb.move_metadata_file(log, package_base_dir)
+
+    assert log.exists()
+    assert f"{rclone_log} already exists. Not moving." in str(exc.value)
+
+
+def test_move_multiple_metadata(package_base_dir: Path, log: Path, md5_manifest: Path):
+    """Test that multiple files are moved successfully"""
+
+    md_files = [log, md5_manifest]
+    pb.move_metadata_files(md_files, package_base_dir)
+
+    for md_file in md_files:
+        assert not md_file.exists()
+        assert (package_base_dir / "metadata" / md_file.name).exists()
+
+
+def test_partial_halt_multiple_metadata(
+    package_base_dir: Path, log: Path, md5_manifest: Path
+):
+    """Test that warning is issued for multiple move if a single metadata move fails"""
+
+    rclone_log = package_base_dir / "metadata" / log.name
+    rclone_log.parent.mkdir()
+    rclone_log.touch()
+
+    md_files = [log, md5_manifest]
+
+    with pytest.raises(Warning) as exc:
+        pb.move_metadata_files(md_files, package_base_dir)
+
+    assert log.exists()
+    assert (
+        f"already exists. Not moving. One or more metadata files may have already been moved to new location"
+        in str(exc.value)
+    )
+
+
+def test_move_payload(package_base_dir: Path, payload: Path):
+    """Test that entirety of payload is moved and hierarchy is preserved"""
+
+    source_contents = [file.relative_to(payload) for file in payload.rglob("*")]
+
+    data_path = package_base_dir / "objects" / "data"
+    pb.move_payload(payload, package_base_dir / "objects")
+
+    # check that source is empty
+    assert not any(payload.iterdir())
+
+    assert data_path.exists()
+
+    # compare contents of data and former source
+    data_contents = [file.relative_to(data_path) for file in data_path.rglob("*")]
+    assert source_contents == data_contents
+
+
+def test_do_not_overwrite_payload(package_base_dir: Path, payload: Path):
+    """Test that no payload file is moved if /data exists"""
+
+    source_contents = [file for file in payload.rglob("*")]
+
+    bag_payload = package_base_dir / "objects" / "data"
+    bag_payload.mkdir(parents=True)
+
+    with pytest.raises(FileExistsError) as exc:
+        pb.move_payload(payload, package_base_dir / "objects")
+
+    # check source has not changed
+    assert source_contents == [file for file in payload.rglob("*")]
+    assert f"{bag_payload} already exists. Not moving files." in str(exc.value)
+
+
+@pytest.fixture
+def bag_payload(package_base_dir: Path, payload: Path):
+    pb.move_payload(payload, package_base_dir)
+    bag_payload = package_base_dir / "data"
+
+    return bag_payload
+
+
+def test_convert_rclone_md5(bag_payload: Path, md5_manifest: Path):
+    pb.convert_rclone_md5_to_bagit_manifest(md5_manifest, bag_payload.parent)
+    bag_md5 = bag_payload.parent / "manifest-md5.txt"
+
+    # Get path to correct payload in data
+    # read md5 and extract filepaths
+    with open(bag_md5) as m:
+        md5_paths = [line.strip().split("  ")[-1] for line in m.readlines()]
+
+    payload_files = [
+        str(path.relative_to(bag_payload.parent)) for path in bag_payload.rglob("*")
+    ]
+    for a_file in md5_paths:
+        assert a_file in payload_files
+
+
+def test_create_bag(package_base_dir: Path, payload: Path, md5_manifest: Path):
+    """Test that all tag files are created and rclone md5sums are correctly converted"""
+
+    bag_path = package_base_dir / "objects"
+
+    # might need further testing of the oxum and manifest converter functions
+    pb.create_bag_in_objects(payload, md5_manifest, package_base_dir)
+
+    assert bagit.Bag(str(bag_path)).validate(completeness_only=True)
+
+
+def test_generate_valid_oxum(transfer_files: Path):
+    """Test that script generates oxum correctly"""
+    # test with entire fixture to text folder recursion
+
+    total_bytes, total_files = pb.get_oxum(transfer_files)
+
+    assert total_bytes == 59286
+    assert total_files == 12
+
+
+def test_validate_valid_bag(transfer_files: Path, caplog):
+    """Test the log message"""
+
+    # create tiny bag for testing
+    object_dir = transfer_files / "objects"
+    object_dir.mkdir()
+    (transfer_files / "rclone.md5").rename(object_dir / "rlcone.md5")
+    test_bag = bagit.make_bag(object_dir)
+
+    pb.validate_bag_in_payload(transfer_files)
+
+    assert f"{test_bag.path} is valid." in caplog.text
+
+
+def test_validate_invalid_bag(transfer_files, caplog):
+    """Test the log message if the bag isn't valid for some reason"""
+
+    object_dir = transfer_files / "objects"
+    object_dir.mkdir()
+    (transfer_files / "rclone.md5").rename(object_dir / "rlcone.md5")
+
+    test_bag = bagit.make_bag(object_dir)
+    print(list(Path(test_bag.path).iterdir()))
+    (Path(test_bag.path) / "bag-info.txt").unlink()
+    pb.validate_bag_in_payload(transfer_files)
+
+    assert (
+        f"{test_bag.path} is not valid. Check the bag manifest and oxum." in caplog.text
+    )
