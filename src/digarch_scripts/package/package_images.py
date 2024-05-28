18177b56dc428d5e69919dcb8f54b4b9784eb139
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2024-05-28 13:52:29 -0400

    tests and code to validate filesets

--- src/digarch_scripts/package/package_images.py
+++ src/digarch_scripts/package/package_images.py
@@ -1,8 +1,16 @@
 import argparse
+import logging
 import re
 from pathlib import Path
 
 import digarch_scripts.package.package_base as pb
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+IMG_EXTS = [".001", ".img", ".dsk"]
+LOG_EXTS = [".log"]
+STREAM_EXTS = [""]
 
 
 def parse_args() -> argparse.Namespace:
@@ -23,54 +31,108 @@ def parse_args() -> argparse.Namespace:
         return id
 
     parser = argparse.ArgumentParser(description="test")
-    parser.add_argument("--images_folder", required=True, type=extant_path, help='Path to working images folder')
-    parser.add_argument("--dest", required=True, type=extant_path, help='Path to packaged images folder')
-    parser.add_argument("--acqid", required=True, type=acq_id, help='ACQ_####')
-    parser.add_argument("--logs_folder", required=False, type=extant_path, help='Path to working logs folder')
-    parser.add_argument("--streams_folder", required=False, type=extant_path, help='Path to working streams folder')
+    parser.add_argument(
+        "--images_folder",
+        required=True,
+        type=extant_path,
+        help="Path to working images folder",
+    )
+    parser.add_argument(
+        "--dest", required=True, type=extant_path, help="Path to packaged images folder"
+    )
+    parser.add_argument("--acqid", required=True, type=acq_id, help="ACQ_####")
+    parser.add_argument(
+        "--logs_folder",
+        required=False,
+        type=extant_path,
+        help="Path to working logs folder",
+    )
+    parser.add_argument(
+        "--streams_folder",
+        required=False,
+        type=extant_path,
+        help="Path to working streams folder",
+    )
 
     return parser.parse_args()
 
 
-def find_category_files(file_groups: dict, source_dir: Path, acq_id: str, category: str) -> dict:
+def find_category_of_carrier_files(
+    carrier_files: dict, acq_id: str, source_dir: Path, exts: list, category: str
+) -> dict:
     for file in source_dir.iterdir():
-        carrier_id_match = re.search(rf'{acq_id}_\d\d\d\d\d\d+', file.name)
+        if not file.suffix in exts:
+            continue
+        carrier_id_match = re.search(rf"{acq_id}_\d\d\d\d\d\d+", file.name)
         if not carrier_id_match:
             continue
         carrier_id = carrier_id_match.group(0)
 
-        if not carrier_id in file_groups:
-            file_groups[carrier_id] = {category: []}
-        elif not category in file_groups[carrier_id]:
-            file_groups[carrier_id][category] = []
+        if not carrier_id in carrier_files:
+            carrier_files[carrier_id] = {category: []}
+        elif not category in carrier_files[carrier_id]:
+            carrier_files[carrier_id][category] = []
 
-        file_groups[carrier_id][category].append(file)
-
-    return file_groups
-
-
-def find_carrier_files(carrier_files: dict, log_dir: Path, images_dir: Path, stream_dir: Path, acq_id: str) -> dict:
-    carrier_files = find_category_files(carrier_files, log_dir, acq_id, 'logs')
-    carrier_files = find_category_files(carrier_files, images_dir, acq_id, 'images')
-    carrier_files = find_category_files(carrier_files, stream_dir, acq_id, 'streams')
+        carrier_files[carrier_id][category].append(file)
 
     return carrier_files
 
 
+def find_carrier_files(
+    acq_id: str, images_dir: Path, log_dir: Path, stream_dir: Path
+) -> dict:
+    carrier_files = find_category_of_carrier_files(
+        {}, acq_id, images_dir, IMG_EXTS, "images"
+    )
+    carrier_files = find_category_of_carrier_files(
+        carrier_files, acq_id, log_dir, LOG_EXTS, "logs"
+    )
+    carrier_files = find_category_of_carrier_files(
+        carrier_files, acq_id, stream_dir, STREAM_EXTS, "streams"
+    )
+
+    if not carrier_files:
+        raise Warning(f"No files found with the acquisition ID {acq_id} in filename")
+
+    return carrier_files
+
+
+def validate_carrier_files(carrier_files):
+    for carrier_name in carrier_files:
+        carrier = carrier_files[carrier_name]
+        missing = []
+        for key in ['images', 'logs', 'streams']:
+            if not key in carrier.keys():
+                missing.append(key)
+
+        if missing:
+            LOGGER.warning(f'The following categories of files were not found for {carrier_name}: {", ".join(missing)} ')
+
+        if 'images' in carrier:
+            for image_file in carrier['images']:
+                if image_file.stat().st_size == 0:
+                    LOGGER.warning(f'The following image file is 0-bytes: {image_file}')
+
+    return
+
 def package_carriers(carrier_files: dict, acq_dir: Path) -> None:
     for carrier, files in carrier_files.items():
         base_dir = pb.create_package_dir(acq_dir, carrier)
-        pb.move_metadata_files(files['logs'], base_dir)
-        pb.move_diskimage_files(files['images'], base_dir)
-        pb.move_stream_files(files['streams'], base_dir)
+        pb.move_metadata_files(files["logs"], base_dir)
+        pb.move_diskimage_files(files["images"], base_dir)
+        pb.move_stream_files(files["streams"], base_dir)
 
 
 def main():
     args = parse_args()
 
-    carrier_files = find_carrier_files({}, args.logs_folder, args.images_folder, args.streams_folder, args.acqid)
-    package_carriers(carrier_files, args.dest)
-
+    carrier_files = find_carrier_files(
+        args.acqid, args.images_folder, args.logs_folder, args.streams_folder
+    )
+    if validate_carrier_files(carrier_files):
+        package_carriers(carrier_files, args.dest)
+    else:
+        LOGGER.error("1 or more errors with files for a carrier. Please address warnings and re-run")
 
 
 if __name__ == "__main__":
--- tests/test_package_images.py
+++ tests/test_package_images.py
@@ -1,10 +1,10 @@
-import digarch_scripts.package.package_images as pi
-
-from pathlib import Path
-import pytest
 import shutil
+from pathlib import Path
 
 import bagit
+import pytest
+
+import digarch_scripts.package.package_images as pi
 
 
 @pytest.fixture
@@ -12,6 +12,7 @@ def transfer_files(tmp_path: Path, request):
     fixture_data = Path(request.module.__file__).parent / "fixtures" / "image"
     shutil.copytree(fixture_data, tmp_path, dirs_exist_ok=True)
     return tmp_path
+
 
 # Test command-line arguments
 @pytest.fixture
@@ -59,7 +60,7 @@ def test_optional_args(
     for i in [3, 4]:
         # remove a pair of list items (arg and value) for each test
         part_args = args[0 : 2 * i + 1] + args[2 * i + 3 :]
-        missing_arg = args[2*i]
+        missing_arg = args[2 * i]
 
         monkeypatch.setattr("sys.argv", part_args)
 
@@ -101,6 +102,144 @@ def test_id_arg_must_match_pattern(
     assert f"bad_id does not match" in stderr
 
 
+def test_carrier_files_found(transfer_files):
+    acq_id = "ACQ_1234"
+
+    carrier_files = pi.find_carrier_files(
+        acq_id,
+        transfer_files / "images",
+        transfer_files / "logs",
+        transfer_files / "streams",
+    )
+
+    carrier1 = f"{acq_id}_123456"
+    assert carrier1 in carrier_files
+    for key in ["images", "logs", "streams"]:
+        assert key in carrier_files[carrier1]
+    for key in carrier_files[carrier1]:
+        for item in carrier_files[carrier1][key]:
+            assert isinstance(item, Path)
+
+
+def test_acqid_not_found(transfer_files):
+    acq_id = "ACQ_1111"
+
+    with pytest.raises(Warning) as exc:
+        pi.find_carrier_files(
+            acq_id,
+            transfer_files / "images",
+            transfer_files / "logs",
+            transfer_files / "streams",
+        )
+
+    assert f"No files found with the acquisition ID {acq_id} in filename" in str(
+        exc.value
+    )
+
+
+def test_file_found(transfer_files):
+    acq_id = "ACQ_1234"
+
+    carrier_files = {}
+    carrier_files = pi.find_category_of_carrier_files(
+        carrier_files, acq_id, transfer_files / "images", [".img"], "images"
+    )
+
+    assert (
+        transfer_files / "images" / "ACQ_1234_123456.img" in carrier_files[f"{acq_id}_123456"]["images"]
+    )
+
+
+def test_ignore_unknown_extension_for_category(transfer_files):
+    acq_id = "ACQ_1234"
+
+    carrier_files = {}
+    carrier_files = pi.find_category_of_carrier_files(
+        carrier_files, acq_id, transfer_files / "images", [".001"], "images"
+    )
+
+    assert f"{acq_id}_123456" not in carrier_files
+
+
+def test_multiple_files_found(transfer_files):
+    acq_id = "ACQ_1234"
+
+    carrier_files = {}
+    carrier_files = pi.find_category_of_carrier_files(
+        carrier_files, acq_id, transfer_files / "logs", [".log"], "logs"
+    )
+
+    assert len(carrier_files[f"{acq_id}_123456"]["logs"]) == 2
+
+
+@pytest.fixture
+def carrier_files(transfer_files):
+    acq_id = "ACQ_1234"
+
+    carrier_files = pi.find_carrier_files(
+        acq_id,
+        transfer_files / "images",
+        transfer_files / "logs",
+        transfer_files / "streams",
+    )
+    return carrier_files
+
+def test_good_validate_carrier(carrier_files, caplog):
+    pi.validate_carrier_files(carrier_files)
+
+    assert not caplog.text
+
+
+@pytest.mark.parametrize("key", ['images', 'logs', 'streams'])
+def test_warn_carrier_with_one_missing_category(carrier_files, key, caplog):
+    carrier_files['ACQ_1234_123456'].pop(key)
+
+    pi.validate_carrier_files(carrier_files)
+
+    assert f'The following categories of files were not found for ACQ_1234_123456: {key}' in caplog.text
+
+
+def test_warn_carrier_with_logs_no_images_or_streams(caplog):
+    carrier_files = {
+        'ACQ_1234_123456': {
+            'logs': [Path('ACQ_1234_123456.log')]
+        }
+    }
+    pi.validate_carrier_files(carrier_files)
+
+    assert f'The following categories of files were not found for ACQ_1234_123456: images, streams' in caplog.text
+
+
+def test_warn_carrier_with_streams_no_images_or_logs(caplog):
+    carrier_files = {
+        'ACQ_1234_123456': {
+            'streams': [Path('ACQ_1234_123456_streams')]
+        }
+    }
+    pi.validate_carrier_files(carrier_files)
+
+    assert f'The following categories of files were not found for ACQ_1234_123456: images, logs' in caplog.text
+
+
+
+def test_warn_and_skip_0_length_image(carrier_files, caplog):
+    carrier_files["ACQ_1234_123457"]["images"][0].unlink()
+    carrier_files["ACQ_1234_123457"]["images"][0].touch()
+    pi.validate_carrier_files(carrier_files)
+
+    assert f'The following image file is 0-bytes: {str(carrier_files["ACQ_1234_123457"]["images"][0])}' in caplog.text
+
+
+def test_warn_streams_missing_a_side():
+    #TODO
+    assert True
+
+
+
+
+
+
+'''
 def test_full_run(
     monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture, args: list
 ):
@@ -113,3 +252,4 @@ def test_full_run(
     assert acq_dir.exists()
 
     assert "ACQ_1234_123456" in [x.name for x in acq_dir.iterdir()]
+'''
