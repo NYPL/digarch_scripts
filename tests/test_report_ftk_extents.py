58f0b1df1b79102446294e3f57843355eb4bad00
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2024-08-13 15:30:45 -0400

    genericize tests

--- tests/test_report_ftk_extents.py
+++ tests/test_report_ftk_extents.py
@@ -11,53 +11,50 @@ except ImportError:
 def parsed_report():
     return etree.parse('tests/fixtures/report/Report.xml')
 
-def test_identify_all_ers(parsed_report):
-    """Function should list every bookmark starting with ER"""
-    ers = rfe.create_component_list(parsed_report)
+@pytest.fixture
+def components(parsed_report):
+    return rfe.create_component_list(parsed_report)
 
-    just_ers = [er[0][-1].split(':')[0] for er in ers]
+def test_identify_all_components(components):
+    """Function should list every bookmark starting with ER and DI"""
+    just_components = [component[0][-1].split(':')[0] for component in components]
 
     for i in range(1, 12):
-        assert f'ER {i}' in just_ers
-    assert 'ER 23' in just_ers
+        assert f'ER {i}' in just_components
+    assert 'ER 23' in just_components
 
-def test_hierarchy_nests_down_correctly(parsed_report):
+def test_hierarchy_nests_down_correctly(components):
     """Function should include organization hierarchy.
     These are not great tests, but I'm not sure what the better strategy would be"""
-    ers = rfe.create_component_list(parsed_report)
-    just_titles = [er[0] for er in ers]
+    just_titles = [component[0] for component in components]
 
     assert ['Extents Test papers', 'Series 1', 'Subseries(1)', 'ER 1: Text, 2023'] in just_titles
     assert ['Extents Test papers', 'Series 1', 'Subseries(1)', 'Subsubseries(2)', 'ER 2: File 15, 2023'] in just_titles
 
-def test_hierarchy_nests_empty_subseries(parsed_report):
+def test_hierarchy_nests_empty_subseries(components):
     """Function should include organization hierarchy including empty levels"""
-    ers = rfe.create_component_list(parsed_report)
-    just_titles = [er[0] for er in ers]
+    just_titles = [component[0] for component in components]
 
     assert ['Extents Test papers', 'Series 1', 'Subseries(1)', 'Subsubseries(2)', 'Subsubsubseries(3)', 'Subsubsubsubseries(4)', 'ER 10: Folder 2, 2023'] in just_titles
 
-def test_hierarchy_nests_up_correctly(parsed_report):
+def test_hierarchy_nests_up_correctly(components):
     """Function should be able to step down in hierarchy"""
-    ers = rfe.create_component_list(parsed_report)
-    just_titles = [er[0] for er in ers]
+    just_titles = [component[0] for component in components]
 
     assert ['Extents Test papers', 'Series 1', 'Subseries(1)', 'Subsubseries(2) the second', 'ER 23: File 17, 2023'] in just_titles
     assert ['Extents Test papers', 'Series 1', 'Subseries(1) the second', 'ER 4: File 18, 2023'] in just_titles
 
-def test_hierarchy_nests_reverse_order_bookmarks(parsed_report):
+def test_hierarchy_nests_reverse_order_bookmarks(components):
     """Function should parse bottom-up hierarchy"""
-    ers = rfe.create_component_list(parsed_report)
-    just_titles = [er[0] for er in ers]
+    just_titles = [component[0] for component in components]
 
     assert ['Extents Test papers', 'Series 2', 'ER 9: File 20,2023'] in just_titles
     assert ['Extents Test papers', 'Series 2', 'Subseries(1) of Series 2', 'ER 8: File 2, 2023'] in just_titles
     assert ['Extents Test papers', 'Series 2', 'Subseries(1) of Series 2', 'Subsubseries(2) of Series 2', 'ER 7: File 19, 2023'] in just_titles
 
-def test_er_outside_of_series(parsed_report):
-    """Function should include capture ERs even if they're not in a series"""
-    ers = rfe.create_component_list(parsed_report)
-    just_titles = [er[0] for er in ers]
+def test_component_outside_of_series(components):
+    """Function should include capture components even if they're not in a series"""
+    just_titles = [component[0] for component in components]
 
     assert ['Extents Test papers', 'ER 10: File 21,2023'] in just_titles
 
@@ -65,34 +62,34 @@ def test_correct_report_many_files(parsed_report):
     """Test if file count and byte count is completed correctly"""
     bookmark_tables = rfe.transform_bookmark_tables(parsed_report)
 
-    er_with_many_files = [['ER 1', 'bk6001']]
-    extents = rfe.add_extents_to_components(er_with_many_files, bookmark_tables)
+    component_with_many_files = [['ER 1', 'bk6001']]
+    extents = rfe.add_extents_to_components(component_with_many_files, bookmark_tables)
 
     # bytes
     assert extents[0][1] == 110
     # files
     assert extents[0][2] == 7
 
-def test_correct_report_on_er_with_folder_bookmarked(parsed_report):
+def test_correct_report_on_component_with_folder_bookmarked(parsed_report):
     """Test if file count and byte count is completed correctly
     when bookmark includes a folder that is bookmarked"""
     bookmark_tables = rfe.transform_bookmark_tables(parsed_report)
 
-    er_with_folder = [['ER 10', 'bk12001']]
-    extents = rfe.add_extents_to_components(er_with_folder, bookmark_tables)
+    component_with_folder = [['ER 10', 'bk12001']]
+    extents = rfe.add_extents_to_components(component_with_folder, bookmark_tables)
 
     # bytes
     assert extents[0][1] == 80
     # files
     assert extents[0][2] == 5
 
-def test_correct_report_on_er_with_folder_not_bookmarked(parsed_report):
+def test_correct_report_on_component_with_folder_not_bookmarked(parsed_report):
     """Test if file count and byte count is completed correctly
     when bookmark includes a folder that isn't bookmarked"""
     bookmark_tables = rfe.transform_bookmark_tables(parsed_report)
 
-    er_with_folder = [['ER 3', 'bk11001']]
-    extents = rfe.add_extents_to_components(er_with_folder, bookmark_tables)
+    component_with_folder = [['ER 3', 'bk11001']]
+    extents = rfe.add_extents_to_components(component_with_folder, bookmark_tables)
 
     # bytes
     assert extents[0][1] == 60
@@ -103,8 +100,8 @@ def test_correct_report_1_file(parsed_report):
     """Test if file count and byte count is completed correctly for one file"""
     bookmark_tables = rfe.transform_bookmark_tables(parsed_report)
 
-    er_with_one_file = [['ER 2', 'bk9001']]
-    extents = rfe.add_extents_to_components(er_with_one_file, bookmark_tables)
+    component_with_one_file = [['ER 2', 'bk9001']]
+    extents = rfe.add_extents_to_components(component_with_one_file, bookmark_tables)
 
     # bytes
     assert extents[0][1] == 16
@@ -112,37 +109,37 @@ def test_correct_report_1_file(parsed_report):
     assert extents[0][2] == 1
 
 def test_warn_on_no_files_in_er(parsed_report, caplog):
-    """Test if warning is logged for empty bookmarks and ER is omitted from report"""
+    """Test if warning is logged for empty bookmarks and component is omitted from report"""
     bookmark_tables = rfe.transform_bookmark_tables(parsed_report)
 
-    er_with_no_files = [[['hier', 'archy', 'list'], 'bk27001', 'ER 5: No Files, 2023']]
+    component_with_no_files = [[['hier', 'archy', 'list'], 'bk27001', 'ER 5: No Files, 2023']]
 
-    extents = rfe.add_extents_to_components(er_with_no_files, bookmark_tables)
+    extents = rfe.add_extents_to_components(component_with_no_files, bookmark_tables)
 
     assert extents == []
 
-    log_msg = f'{er_with_no_files[0][-1]} does not contain any files. It will be omitted from the report.'
+    log_msg = f'{component_with_no_files[0][-1]} does not contain any files. It will be omitted from the report.'
     assert log_msg in caplog.text
 
 def test_warn_on_a_no_byte_file_in_er(parsed_report, caplog):
-    """Test if warning is logged for empty files in an ER"""
+    """Test if warning is logged for empty files in an component"""
     bookmark_tables = rfe.transform_bookmark_tables(parsed_report)
 
-    er_with_no_bytes = [[['hier', 'archy', 'list'], 'bk28001', 'ER 6: Zero Length, 2023']]
-    rfe.add_extents_to_components(er_with_no_bytes, bookmark_tables)
-    log_msg = f'{er_with_no_bytes[0][-1]} contains the following 0-byte file: file00.txt. Review this file with the processing archivist.'
+    component_with_no_bytes = [[['hier', 'archy', 'list'], 'bk28001', 'ER 6: Zero Length, 2023']]
+    rfe.add_extents_to_components(component_with_no_bytes, bookmark_tables)
+    log_msg = f'{component_with_no_bytes[0][-1]} contains the following 0-byte file: file00.txt. Review this file with the processing archivist.'
     assert log_msg in caplog.text
 
 def test_warn_on_no_bytes_in_er(parsed_report, caplog):
-    """Test if warning is logged for bookmarks with 0 bytes total and ER is omitted from report"""
+    """Test if warning is logged for bookmarks with 0 bytes total and component is omitted from report"""
     bookmark_tables = rfe.transform_bookmark_tables(parsed_report)
 
-    er_with_no_bytes = [[['hier', 'archy', 'list'], 'bk28001', 'ER 6: Zero Length, 2023']]
-    extents = rfe.add_extents_to_components(er_with_no_bytes, bookmark_tables)
+    component_with_no_bytes = [[['hier', 'archy', 'list'], 'bk28001', 'ER 6: Zero Length, 2023']]
+    extents = rfe.add_extents_to_components(component_with_no_bytes, bookmark_tables)
 
     assert extents == []
 
-    log_msg = f'{er_with_no_bytes[0][-1]} contains no files with bytes. This component is omitted from report. Review this component with the processing archivist.'
+    log_msg = f'{component_with_no_bytes[0][-1]} contains no files with bytes. This component is omitted from report. Review this component with the processing archivist.'
     assert log_msg in caplog.text
 
 
@@ -153,22 +150,22 @@ def test_extract_collection_name_from_report(parsed_report):
     assert coll_name == 'M12345 Extents Test'
 
 @pytest.fixture
-def ers_with_extents_list(parsed_report):
-    ers = rfe.create_component_list(parsed_report)
+def components_with_extents_list(parsed_report):
+    components = rfe.create_component_list(parsed_report)
     bookmark_tables = rfe.transform_bookmark_tables(parsed_report)
-    ers_with_extents = rfe.add_extents_to_components(ers, bookmark_tables)
+    components_with_extents = rfe.add_extents_to_components(components, bookmark_tables)
 
-    return ers_with_extents
+    return components_with_extents
 
-def test_json_objects_contains_expected_fields(ers_with_extents_list):
+def test_json_objects_contains_expected_fields(components_with_extents_list):
     """Test if final report aligns with expectations for ASpace import"""
 
     full_dict = {'title': 'slug', 'children': []}
-    for er in ers_with_extents_list:
-        rfe.create_report(er, full_dict)
+    for component in components_with_extents_list:
+        rfe.create_report(component, full_dict)
 
-    def recursive_validator(er_dict):
-        for key, value in er_dict.items():
+    def recursive_validator(component_dict):
+        for key, value in component_dict.items():
             if key == 'title':
                 assert type(value) is str
             elif key == 'children':
@@ -188,30 +185,28 @@ def test_json_objects_contains_expected_fields(ers_with_extents_list):
 
     recursive_validator(full_dict)
 
-def test_skipped_ER_number_behavior(parsed_report, caplog):
-    """Test if script flags when ER numbering is not sequential"""
-    ers = rfe.create_component_list(parsed_report)
+
+def test_skipped_number_behavior(parsed_report, caplog):
+    """Test if script flags when component numbering is not sequential"""
+    components = rfe.create_component_list(parsed_report)
 
     for i in range(13, 23):
         assert f'Collection ER component range is numbered 1 to 23. {i} is skipped. Review the bookmarks with the processing archivist' in caplog.text
 
 
-def test_ER_missing_number_behavior(parsed_report, caplog):
-    """Test if script flags when ER number is reused"""
-    ers = rfe.create_component_list(parsed_report)
-    ers[0][2] = "ER ?: File 21,2023"
+def test_component_missing_number_behavior(components, caplog):
+    """Test if script flags when component number is reused"""
+    components[0][2] = "ER ?: File 21,2023"
 
-    rfe.audit_components(ers)
+    rfe.audit_components(components)
 
     log_msg = f'Component is missing a number: ER ?: File 21,2023. Review the bookmarks with the processing archivist'
     assert log_msg in caplog.text
 
 
-def test_repeated_ER_number_behavior(parsed_report, caplog):
-    """Test if script flags when ER number is reused"""
-    ers = rfe.create_component_list(parsed_report)
-
-    rfe.audit_components(ers)
+def test_repeated_component_number_behavior(components, caplog):
+    """Test if script flags when component number is reused"""
+    rfe.audit_components(components)
 
     log_msg = f'ER 10 is used multiple times: ER 10: File 21,2023, ER 10: Folder 2, 2023. Review the bookmarks with the processing archivist'
 
@@ -223,10 +218,10 @@ def expected_json():
         report = json.load(f)
     return report
 
-def test_create_correct_json(ers_with_extents_list, expected_json):
+def test_create_correct_json(components_with_extents_list, expected_json):
     """Test that final report matches total expectations"""
     dct = {'title': 'coll', 'children': []}
-    for er in ers_with_extents_list:
-        dct = rfe.create_report(er, dct)
+    for component in components_with_extents_list:
+        dct = rfe.create_report(component, dct)
 
     assert dct == expected_json
